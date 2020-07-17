05f5700f2c21943629dbaa4cea9cbf29fe910085
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-07-17 10:07:54 +0200

    fix(cart): fix cart width, update translations

--- src/components/Cart/Cart/styles.ts
+++ src/components/Cart/Cart/styles.ts
@@ -1,10 +1,14 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { rightDrawerWidth } from "../../Layout/constants";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      maxWidth: rightDrawerWidth - 1,
+    },
     title: {
-      padding: theme.spacing(2)
-    }
+      padding: theme.spacing(2),
+    },
   })
 );
 
