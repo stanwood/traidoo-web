5c31a4b5c8044389295088ff59f06c640f1f6f9e
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-09-09 13:35:54 +0200

    fix(permissions): redirect user to login page when access token is not available

--- src/core/routes/PrivateRoute.tsx
+++ src/core/routes/PrivateRoute.tsx
@@ -1,33 +1,15 @@
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Grid from "@material-ui/core/Grid";
-import React, { useContext } from "react";
+import React from "react";
 import { Redirect, Route, RouteProps } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext/context";
+import { getAccessToken } from "../../api/jwt";
 
 const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
-  const { user, isIdle, isLoading } = useContext(UserContext);
   const { children, ...restProps } = props;
-
-  if (isIdle || isLoading) {
-    return (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <CircularProgress />
-      </Grid>
-    );
-  }
 
   return (
     <Route
       {...restProps}
       render={({ location }) =>
-        user.id ? (
+        getAccessToken() ? (
           children
         ) : (
           <Redirect
