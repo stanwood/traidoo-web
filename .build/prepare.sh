d0d5fa633198638673adc35cdaaf0439ff1f1726
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-10-12 05:24:54 +0200

    build(google-cloud-build): move all deployment scripts to .build directory

--- .build/prepare.sh
+++ .build/prepare.sh
@@ -3,5 +3,5 @@
 sed -i -e "s/__PAGE_TITLE__/$CLIENT_NAME/g" public/index.html
 sed -i -e "s/__PAGE_DESCRIPTION__/$CLIENT_NAME/g" public/index.html
 wget $FAVICON_URL -O public/favicon.png
-node src/scripts/generate_favicons.js
+node .build/generate_favicons.js
 sed -i -e "s|__FAVICONS__|$(tr -d '\n' < public/favicons.html)|g" public/index.html
