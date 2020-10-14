b06a48b852e1609b488bf5bc49292e8f485289f3
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-10-14 08:58:21 +0200

    fix(layout): hide favicons string if not provided

--- .build/prepare.sh
+++ .build/prepare.sh
@@ -4,4 +4,4 @@ sed -i -e "s/__PAGE_TITLE__/$CLIENT_NAME/g" public/index.html
 sed -i -e "s/__PAGE_DESCRIPTION__/$CLIENT_NAME/g" public/index.html
 wget $FAVICON_URL -O public/favicon.png
 node .build/generate_favicons.js
-sed -i -e "s|__FAVICONS__|$(tr -d '\n' < public/favicons.html)|g" public/index.html
+sed -i -e "s|<!-- __FAVICONS__ -->|$(tr -d '\n' < public/favicons.html)|g" public/index.html
