d6f432875ef8d0385a0cef8bb0b3000f0b2a7f99
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-11-03 17:47:10 +0100

    feat(cart): display message when price is too low (MCS-2061)

--- src/components/Cart/Root/index.tsx
+++ src/components/Cart/Root/index.tsx
@@ -1,18 +1,57 @@
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import Tooltip from "@material-ui/core/Tooltip";
 import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
-import { Link as RouterLink } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { getSettingsRequest } from "../../../api/queries/settings/settings";
 import { CartContext } from "../../../contexts/CartContext/context";
 import Cart from "../Cart/component";
 import CartItem from "../CartItem/component";
 import { useCartStyles } from "./styles";
 
-const RenderCart: React.FC = () => {
+const RenderCart = () => {
   const classes = useCartStyles();
   const { cart, clear, cartTotal } = useContext(CartContext);
   const { t } = useTranslation();
+  const { data: settings, isLoading } = useQuery(
+    "/settings",
+    getSettingsRequest
+  );
+
+  const isPriceTooLow = React.useMemo(() => {
+    if (cartTotal === 0) {
+      return false;
+    }
+
+    if (!settings?.minPurchaseValue) {
+      return false;
+    }
+
+    if (settings.minPurchaseValue > cartTotal) {
+      return true;
+    }
+
+    return false;
+  }, [settings, cartTotal]);
+
+  const disableButton = React.useMemo(() => {
+    if (isLoading) {
+      return true;
+    }
+
+    if (!settings?.minPurchaseValue) {
+      return true;
+    }
+
+    if (settings.minPurchaseValue > cartTotal) {
+      return true;
+    }
+
+    return false;
+  }, [isLoading, cartTotal, settings]);
 
   const renderItems = () => {
     return cart.products.map((product) => {
@@ -44,15 +83,28 @@ const RenderCart: React.FC = () => {
         >
           {t("deleteAll")}
         </Button>
-        <Button
-          component={RouterLink}
-          to="/checkout"
-          variant="contained"
-          color="primary"
-          disabled={cart.products.length === 0}
-        >
-          {t("checkout")} {cartTotal.toFixed(2)}€
-        </Button>
+        {isPriceTooLow ? (
+          <Tooltip
+            title={`${t("checkoutPriceTooLow")} ${settings?.minPurchaseValue}€`}
+            arrow
+          >
+            <span>
+              <Button variant="contained" color="primary" disabled>
+                {t("checkout")} {cartTotal.toFixed(2)}€
+              </Button>
+            </span>
+          </Tooltip>
+        ) : (
+          <Button
+            component={Link}
+            to="/checkout"
+            variant="contained"
+            color="primary"
+            disabled={disableButton}
+          >
+            {t("checkout")} {cartTotal.toFixed(2)}€
+          </Button>
+        )}
       </Grid>
     </Grid>
   );
