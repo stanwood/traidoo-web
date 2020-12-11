bcffacaf9f9d530211d28570949b80f5bed0da68
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-12-11 14:07:38 +0100

    feat(categories) display small images in the same line

---
+++ src/components/Categories/GridListItem.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import GridListTile from "@material-ui/core/GridListTile";
+import { Img } from "react-image";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+export const useGridListImageItemStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    tile: { maxWidth: "100%" },
+    image: {
+      width: "100%",
+      height: "auto",
+      maxHeight: "78px",
+      maxWidth: "100%",
+    },
+  })
+);
+
+interface GridListImageItemProps {
+  imageItem: string;
+}
+
+const GridListImageItem = (props: GridListImageItemProps) => {
+  const classes = useGridListImageItemStyles();
+  const [imageUrl, numberOfColumns] = props.imageItem.split(";");
+
+  return (
+    <GridListTile
+      key={imageUrl}
+      cols={Number(numberOfColumns)}
+      className={classes.tile}
+    >
+      <Img src={imageUrl} className={classes.image} />
+    </GridListTile>
+  );
+};
+
+export default GridListImageItem;
